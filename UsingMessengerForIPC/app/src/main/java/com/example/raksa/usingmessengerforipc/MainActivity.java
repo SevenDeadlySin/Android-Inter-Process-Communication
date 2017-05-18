1715a795ba05adfa3b6667746d836f9ddd4c1cad
BamBo <raksameth@gmail.com>
2017-05-18 21:50:36 +0700

    Modified Main Layout and Add Event For Button

--- UsingMessengerForIPC/app/src/main/java/com/example/raksa/usingmessengerforipc/MainActivity.java
+++ UsingMessengerForIPC/app/src/main/java/com/example/raksa/usingmessengerforipc/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.raksa.usingmessengerforipc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onAddButton(View view) {
+
+    }
+
+    public void onBindButton(View view) {
+        
+    }
+
+    public void onUnbindButton(View view) {
+
     }
 }
