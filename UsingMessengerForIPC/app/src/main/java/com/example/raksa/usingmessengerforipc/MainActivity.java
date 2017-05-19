910d0cc1f46ee6133065f3f988d2b25124398b46
BamBo <raksameth@gmail.com>
2017-05-19 17:48:56 +0700

    Perform IPC Operation Between Component and Service In Different Process

--- UsingMessengerForIPC/app/src/main/java/com/example/raksa/usingmessengerforipc/MainActivity.java
+++ UsingMessengerForIPC/app/src/main/java/com/example/raksa/usingmessengerforipc/MainActivity.java
@@ -3,21 +3,58 @@ package com.example.raksa.usingmessengerforipc;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText editTextValue1;
+    EditText editTextValue2;
+    TextView textViewResult;
+
     boolean isBind = false;
 
+    private class ActivityHandler extends Handler{
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 43:
+
+                    Bundle bundle = msg.getData();
+                    int result = bundle.getInt("result",0);
+                    textViewResult.setText(String.valueOf(result));
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+
+        }
+    }
+
+    ActivityHandler activityHandler = new ActivityHandler();
+
+
+    Messenger mActivityMessenger = new Messenger(activityHandler);
+
+
+    Messenger mServiceMessenger = null;
 
     ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder binder) {
             isBind = true;
+            mServiceMessenger = new Messenger(binder);
+
         }
 
         @Override
@@ -26,18 +63,39 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewResult = (TextView) findViewById(R.id.textViewResult);
+        editTextValue1 = (EditText) findViewById(R.id.editTextValue1);
+        editTextValue2 = (EditText) findViewById(R.id.editTextValue2);
 
 
     }
 
     public void onAddButton(View view) {
+
+        if (isBind){
+
+            Message messageToService = Message.obtain(null,43);
+
+            Bundle bundle = new Bundle();
+            bundle.putInt("value1",Integer.valueOf(editTextValue1.getText().toString()));
+            bundle.putInt("value2",Integer.valueOf(editTextValue2.getText().toString()));
+            bundle.putParcelable("activityMessenger",mActivityMessenger);
+            messageToService.setData(bundle);
+
+            try {
+                mServiceMessenger.send(messageToService);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Please Bind The Service First!",Toast.LENGTH_SHORT).show();
+        }
 
     }
 
--- UsingMessengerForIPC/app/src/main/java/com/example/raksa/usingmessengerforipc/MyService.java
+++ UsingMessengerForIPC/app/src/main/java/com/example/raksa/usingmessengerforipc/MyService.java
@@ -3,10 +3,12 @@ package com.example.raksa.usingmessengerforipc;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.annotation.Nullable;
 
 /**
@@ -21,7 +23,24 @@ public class MyService extends Service {
         public void handleMessage(Message msg) {
             switch (msg.what){
                 case 43:
-                break;
+
+                    Bundle bundle = msg.getData();
+                    int result = addService(bundle.getInt("value1",0),bundle.getInt("value2",0));
+
+                    Messenger activityMessenger = bundle.getParcelable("activityMessenger");
+
+                    Message messageToActivity = Message.obtain(null,43);
+                    Bundle toActivityBundle = new Bundle();
+                    bundle.putInt("result",result);
+                    messageToActivity.setData(bundle);
+
+                    try {
+                        activityMessenger.send(messageToActivity);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
 
                 default:
                     super.handleMessage(msg);
